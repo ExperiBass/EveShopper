fdd96b1cf86620bad11adef567b7455e1ffb29da
GingkathFox <gingkathfox@gmail.com>
2019-03-24 13:00:51 -0400

    closes #1

--- Electron/Functions/getOrders.js
+++ Electron/Functions/getOrders.js
@@ -16,29 +16,24 @@ async function getOrders(regID, buySell, item) {
                   regID + '/orders/?datasource=tranquility&order_type=' + 
                   buySell +
                   '&page=1&type_id=' + item)
-                .then(response => {
+                 .then(response => {
 
                   const data = response.data
                   var info;
+                  
                   var volRem = data.volume_remain // for remaining volume of items in station (buy option only)
 
-                  var s = getStations(data[0].location_id)
-                  var name;
-                  var promiseName = Promise.resolve(s)
+                  for (var i = 0; i < data.length; i++) {
 
-                  promiseName.then(function(value) {
-                    console.log('line 25: ' + value)
-                    name = value
-                    console.log('Line 27: ' + name)
-                  })
-                  setTimeout(() => { console.log('From line 29, getOrders: ' + name) }, 1000) 
-                  // this returns a promise instead of the value of `name`
-
-                  /*for (var i = 0; i < data.length; i++) {
-                    var stations = setTimeout(() => { getStations(data[i].location_id) }, 1000)
                     var prices = data[i].price
-                    info += '\n Station: ' + stations
-                  }*/
+                    console.log(data[i].location_id)
+                    var stations;
+
+                    setTimeout(() => { getStations(data[i].location_id).then(function(value) { stations += value }) }, 1000)
+
+
+                    setTimeout(() => { info += '\n Station: ' + stations }, 1000)
+                  }
                   //console.log(info)
                 })
   }
--- Electron/Functions/getStations.js
+++ Electron/Functions/getStations.js
@@ -11,27 +11,14 @@ async function getStations(location) {
     await axios.get('https://esi.evetech.net/latest/universe/stations/' + location + '/?datasource=tranquility')
                   .then(response => {
                     const data = response.data
-
-                      //promiseName = data.name
+                    console.log(data)
                     promiseName = Promise.resolve(data.name)
-                    console.log(name)
+
                   })
                   .catch(error => {
                   console.log(error)
                   alert(error)
                   })
 
-       /* promiseName.then(function(value) {
-          console.log('line 22: ' + value)
-          name = value
-          console.log('Line 24: ' + name)
-        })*/
-
-  setTimeout(() => { return name }, 100) // this logs the name perfectly
-
-/* function returnV(a) {
-  console.log('from `returnV`: ' + a)
-  return a // is passed back to `getOrders.js`
-}*/
-
+return promiseName
 }
