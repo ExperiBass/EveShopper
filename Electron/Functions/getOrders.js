5a135d131dd42f631b5f3ff6fdc239a76ca6d576
GingkathFox <gingkathfox@gmail.com>
2019-03-24 02:04:29 -0400

    indenting

--- Electron/Functions/getOrders.js
+++ Electron/Functions/getOrders.js
@@ -5,34 +5,39 @@ const getStations = require('./getStations')
 
 async function getOrders(regID, buySell, item) {
 
-    // checking if a radio button was pressed
-    if (buySell == undefined) {
-              document.getElementById('warning').innerHTML = 'Choose either "Buy" or "Sell"!'
-              return;
-            }
-        // Getting the orders
-     await axios.get('https://esi.evetech.net/latest/markets/' + regID + '/orders/?datasource=tranquility&order_type=' + buySell +
-                '&page=1&type_id=' + item)
-          .then(response => {
-              const data = response.data
-              var info;
+  // checking if a radio button was pressed
+  if (buySell == undefined) {
+    document.getElementById('warning').innerHTML = 'Choose either "Buy" or "Sell"!'
+    return;
+  }
 
-              var s = getStations(data[0].location_id)
-              var name;
-              var promiseName = Promise.resolve(s)
+  // Getting the orders
+  await axios.get('https://esi.evetech.net/latest/markets/' + 
+                  regID + '/orders/?datasource=tranquility&order_type=' + 
+                  buySell +
+                  '&page=1&type_id=' + item)
+                .then(response => {
 
-              promiseName.then(function(value) {
-                console.log('line 25: ' + value)
-                name = value
-                console.log('Line 27: ' + name)
-              })
-              setTimeout(() => { console.log('From line 29, getOrders: ' + name) }, 1000) // this returns a promise instead of the value of `name`
+                  const data = response.data
+                  var info;
 
-              /*for (var i = 0; i < data.length; i++) {
-                var stations = setTimeout(() => { getStations(data[i].location_id) }, 1000)
-                var prices = data[i].price
-                info += '\n Station: ' + stations
-              }*/
-              //console.log(info)
-          })
+                  var s = getStations(data[0].location_id)
+                  var name;
+                  var promiseName = Promise.resolve(s)
+
+                  promiseName.then(function(value) {
+                    console.log('line 25: ' + value)
+                    name = value
+                    console.log('Line 27: ' + name)
+                  })
+                  setTimeout(() => { console.log('From line 29, getOrders: ' + name) }, 1000) 
+                  // this returns a promise instead of the value of `name`
+
+                  /*for (var i = 0; i < data.length; i++) {
+                    var stations = setTimeout(() => { getStations(data[i].location_id) }, 1000)
+                    var prices = data[i].price
+                    info += '\n Station: ' + stations
+                  }*/
+                  //console.log(info)
+                })
   }
--- Electron/Functions/getStations.js
+++ Electron/Functions/getStations.js
@@ -9,17 +9,17 @@ async function getStations(location) {
 
     // Get the stations location
     await axios.get('https://esi.evetech.net/latest/universe/stations/' + location + '/?datasource=tranquility')
-          .then(response => {
-            const data = response.data
+                  .then(response => {
+                    const data = response.data
 
-              //promiseName = data.name
-             promiseName = Promise.resolve(data.name)
-             console.log(name)
-          })
-          .catch(error => {
-          console.log(error)
-          alert(error)
-        })
+                      //promiseName = data.name
+                    promiseName = Promise.resolve(data.name)
+                    console.log(name)
+                  })
+                  .catch(error => {
+                  console.log(error)
+                  alert(error)
+                  })
 
        /* promiseName.then(function(value) {
           console.log('line 22: ' + value)
@@ -27,11 +27,11 @@ async function getStations(location) {
           console.log('Line 24: ' + name)
         })*/
 
-    setTimeout(() => { return name }, 100) // this logs the name perfectly
+  setTimeout(() => { return name }, 100) // this logs the name perfectly
 
-   /* function returnV(a) {
-      console.log('from `returnV`: ' + a)
-      return a // is passed back to `getOrders.js`
-    }*/
+/* function returnV(a) {
+  console.log('from `returnV`: ' + a)
+  return a // is passed back to `getOrders.js`
+}*/
 
 }
