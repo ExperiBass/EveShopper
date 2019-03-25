470585c36b4e5ca1afaef0870be2f6d54978d11e
GingkathFox <gingkathfox@gmail.com>
2019-03-25 00:13:09 -0400

    closes #2

--- Electron/Functions/getOrders.js
+++ Electron/Functions/getOrders.js
@@ -22,31 +22,32 @@ async function getOrders(regID, buySell, item) {
                   console.log(error.response.data)
                 })
 
-                var j = 0
+                var j = 20
 
                 var get = setInterval(getStations, 1000, data[j].location_id)
                 var count = setInterval(incr, 1010)
                 
                 async function incr() {
-
+                  j++
                   if (j > 24) {
-                      clearInterval(get)
-                      clearInterval(count)
-                      return
-                  }
+                    clearInterval(get)
+                    clearInterval(count)
+                    return
+                }
                   const currentData = data[j];
                   var station = await getStations(currentData.location_id);
-                  var price = currentData.price, remVol
+                  var price = currentData.price, remVol, minVol
 
-                  j++
+                  //j++
                   if (buySell == 'buy') {
                     remVol = currentData.volume_remain
 
                     console.log(`${j} , Location ID: ${currentData.location_id} Station: ${station}, 
                                 Price: ${price} ISK, Remaining Volume: ${remVol}`)
                   } else {      
+                    sminVol = currentData.min_volume
                     console.log(`${j} , Location ID: ${currentData.location_id} Station: ${station}, 
-                    Price: ${price} ISK`)
+                    Price: ${price} ISK, Minimum Volume: ${minVol}`)
                   }
                   
               }
