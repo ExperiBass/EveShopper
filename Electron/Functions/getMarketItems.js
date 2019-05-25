c902be9d5bee4ee7b9a0110819919230aa935d3c
GingkathFox <gingkathfox@gmail.com>
2019-05-25 09:34:59 -0400

    unhandled promise rejections

--- Electron/Functions/getMarketItems.js
+++ Electron/Functions/getMarketItems.js
@@ -1,6 +1,7 @@
 module.exports = getMarketItems
 
 let axios = require('axios')
+const err = require('./err')
 const link = 'https://esi.evetech.net/latest/'
 let marketItems = []
 async function sleep(millis) {
@@ -12,36 +13,37 @@ async function sleep(millis) {
     let data;
 
      axios.get(`${link}markets/groups/?datasource=tranquility`)
-        .then(response => {
+        .then(async (response) => {
             data = response.data
             marketGroups = data.slice(0)
+            console.log(marketGroups.length)
             
             for (let i = 0; i < marketGroups.length; i++) {
                  axios.get(`${link}markets/groups/${marketGroups[i]}/?datasource=tranquility&language=en-us`)
                     .then(response => {
                         let data = response.data
                         let IDs = data.types
-
-                        if (IDs == []) {
-                            return
-                        }
-
-                        for (let j = 0; j < IDs; j++) {
-                             axios.get(`${link}universe/types/${IDs[j]}/?datasource=tranquility&language=en-us`)
-                                .then(response => {
-                                    let data = response.data
-                                    marketItems.push(data.name)
-                                })
-                           await sleep(150)
-                        }
-                    })
+                        //console.log(IDs)
+                        
+                        getNames(IDs)
+                    }).catch( e => { console.error(e) } )
+                await sleep(200)
             }
         })
-        .catch(error => {
-            console.log(error)
-            return
-        })
+        .catch( e => { console.error(e) } )
 
 }
 
+async function getNames(array) {
+
+    if (array[0] = undefined || null) {
+        return
+    }
+    let res = await axios.post(`${link}universe/names/?datasource=tranquility`, array)
+    
+    for (let j = 0; j < res.data; j++) {
+        marketItems.push(res.data[j].name)
+        console.log(marketItems)
+    }
+}
 console.log(getMarketItems())
