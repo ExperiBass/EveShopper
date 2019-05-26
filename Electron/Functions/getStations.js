80f8169ac03cbb121a748d21e7ae438ef670a6d3
Perdog <jacob_perry@hotmail.ca>
2019-05-26 06:43:26 -0400

    Error spam in console
    *All* NPC stations have IDs that are much much lower then *all* upwells, so you can add a check to prevent the app from trying to use the wrong endpoint for upwell structures, which was spamming a bunch of errors

--- Electron/Functions/getStations.js
+++ Electron/Functions/getStations.js
@@ -5,12 +5,13 @@ module.exports = getStations
 const axios = require('axios')
 const err = require('./err')
 
-async function getStations(location) {
+async function getStations(locationID) {
 
   let promiseName;
-
-    // Get the stations location
-    await axios.get(`https://esi.evetech.net/latest/universe/stations/${location}/?datasource=tranquility`)
+	
+	if (locationID < 1000000000000) {
+		// Get the stations location
+		await axios.get(`https://esi.evetech.net/latest/universe/stations/${locationID}/?datasource=tranquility`)
                   .then(response => {
                     const data = response.data
 
@@ -19,6 +20,9 @@ async function getStations(location) {
                   .catch(error => { 
                     err(error, 'Function: getStations()')
                   })
+	} else {
+		promiseName = "Upwell market";
+	}
 
 return promiseName
 }
